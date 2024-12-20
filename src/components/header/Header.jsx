fdc4c3e55490aaaf791ff00b5f322d54f56ece37
harry <tl9434@naver.com>
2024-12-20 21:49:56 +0900

    set up folders

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import Score from "./score/Score";
+
+const Header = () => {
+  return (
+    <div>
+      Header
+      <Score />
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import Cards from "./cards/Cards";
+
+const Main = () => {
+  return (
+    <div>
+      Main
+      <Cards />
+    </div>
+  )
+}
+
+export default Main;
